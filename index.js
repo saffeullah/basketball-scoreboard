3d3bdc35973b19e353dcb1fe7121b4955a5bd375
Safi Ullah <41166092+saffeullah@users.noreply.github.com>
2023-05-11 16:11:35 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let guestEl = document.getElementById("guest-score")
+let homeEl = document.getElementById("home-score")
+
+function score(team, score){
+    let homeScore = parseInt(homeEl.textContent)
+    let guestScore = parseInt(guestEl.textContent)
+     if(team == 'home' && score == 0) {
+        
+         homeEl.textContent = 0
+    } 
+    if(team == 'home' && score == 1) {
+        
+         homeEl.textContent = ++homeScore
+    }
+    if(team == 'home' && score == 2) {
+         homeEl.textContent = homeScore+2
+    }
+     if(team == 'home' && score == 3) {
+         homeEl.textContent = homeScore+3
+    }
+    if(team == 'guest' && score == 0) {
+        
+         guestEl.textContent = 0
+    } 
+    if(team == 'guest' && score == 1) {
+        
+         guestEl.textContent = ++guestScore
+    }
+    if(team == 'guest' && score == 2) {
+         guestEl.textContent = guestScore+2
+    }
+     if(team == 'guest' && score == 3) {
+         guestEl.textContent = guestScore+3
+    }
+}
+
+
+
